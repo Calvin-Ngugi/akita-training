6802fc0009e84a43f898a8759cde88931e01e520
calvin <ncalvin67@gmail.com>
2023-07-10 18:25:01 +0300

    :sparkles: Adding home and add-todo components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
+import { HomeComponent } from './home/home.component';
+import { AddTodoComponent } from './add-todo/add-todo.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, AddTodoComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
