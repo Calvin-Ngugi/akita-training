861b4c6bc463c44cc062b87689b2527851fb804f
calvin <ncalvin67@gmail.com>
2023-07-12 16:42:47 +0300

    :zap: :sparkles: Adding routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,16 @@ import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { HomeComponent } from './home/home.component';
 import { AddTodoComponent } from './add-todo/add-todo.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, AddTodoComponent],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AddTodoComponent,
+    NotFoundComponent,
   ],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Route } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AddTodoComponent } from './add-todo/add-todo.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'add-todo',
+        component: AddTodoComponent
+    },
+    {
+        path: '**',
+        component: NotFoundComponent
+    }
+];
