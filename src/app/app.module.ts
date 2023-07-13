24d075681da969c5f00cb296b35d192525959c8c
calvin <ncalvin67@gmail.com>
2023-07-13 15:15:23 +0300

    ➕ angular material dependency added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { AddTodoComponent } from './add-todo/add-todo.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -15,8 +16,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AddTodoComponent,
     NotFoundComponent,
   ],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes), BrowserAnimationsModule],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes), BrowserAnimationsModule, MatToolbarModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
