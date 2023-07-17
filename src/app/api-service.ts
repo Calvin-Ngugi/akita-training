846e9f62256674516a3338c4565bdbf9dcd1933e
calvin <ncalvin67@gmail.com>
2023-07-17 11:16:13 +0300

    Updating the api service

--- src/app/api-service.ts
+++ src/app/api-service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { Observable, map } from "rxjs";
 import { Todo } from "./todo-model";
+import { environment } from "src/environments/environment";
 
 Injectable({
     providedIn: 'root'
@@ -15,4 +16,18 @@ export class ApiService{
     addTodo(title: string, description: string): Observable<Todo>{
         return this.http.post<Todo>(this.baseUrl, { title, description });
     }
+
+    getTodos(): Observable<Todo[]> {
+        return this.http.get<Todo[]>(this.baseUrl).pipe(
+            map((res:any) => res.data)
+        );
+    }
+
+    deleteTodo(id: string): Observable<Todo[]> {
+        return this.http.delete<Todo[]>(`${this.baseUrl}/${id}`);
+    }
+
+    updateTodo(id: string, changes: Todo): Observable<Todo[]> {
+        return this.http.put<Todo[]>(`${this.baseUrl}/${id}`, changes);
+     }
 }
